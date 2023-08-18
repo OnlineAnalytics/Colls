1e1a522ddc077b6ed4661583f6e49c08459e43fa
OnlineAnalytics <116591933+OnlineAnalytics@users.noreply.github.com>
2023-08-18 21:13:30 +0100

    a

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ with st.sidebar:
     option = st.selectbox(' ',('For','Against'))
 
 if option == 'For':
- top = st.selectbox("Select 'All' to see all shots, or a specific player.", options=('All', df[df['Team']=='Colls'].Player.unique().tolist()))
+ top = st.selectbox("Select 'All' to see all shots, or a specific player.", options=(['All', df[df['Team']=='Colls'].Player.unique().tolist()]))
  
  if top == 'All':
   d = df[df['Team']=='Colls']
